a8294f9d4785a857a5d40d2c52767b6fd6aff9bb
asith-w <asithgit@gmail.com>
2019-01-07 01:00:14 +0545

    using prism eventAggregator

---
+++ Use Prism EventAggregator/FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,27 @@
+﻿using FriendOrganizer.DataAccess;
+using FriendOrganizer.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FriendOrganizer.UI.Data
+{
+  public class FriendDataService : IFriendDataService
+  {
+    private Func<FriendOrganizerDbContext> _contextCreator;
+
+    public FriendDataService(Func<FriendOrganizerDbContext> contextCreator)
+    {
+      _contextCreator = contextCreator;
+    }
+    public async Task<Friend> GetByIdAsync(int friendId)
+    {
+      using (var ctx = _contextCreator())
+      {
+        return await ctx.Friends.AsNoTracking().SingleAsync(f => f.Id == friendId);
+      }
+    }
+  }
+}
